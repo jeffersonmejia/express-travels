000f24150decefeae06936356a26e2d8fe1d7d82
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-04-25 17:30:35 -0500

    update git remote script

--- src/scripts/git-remote.sh
+++ src/scripts/git-remote.sh
@@ -2,6 +2,11 @@
 clear
 error_message="\e[31merror -\e[0m"
 success_message="\e[32msuccess\e[0m -"
+event_message="\e[35mevent\e[0m -"
+wait_message="\e[36mwait\e[0m -"
+
+
+
 function print_help {
 	echo -e "Use: npm run gh [commit-message]"
 }
@@ -25,6 +30,7 @@ function check_git_repository {
 
 function push_commit {
 	if git status --porcelain | grep -q "^[ M A D R C]"; then
+		echo -e "$wait_message Pushing commit..."
 		git add . && git commit -m "$1"
 		git pull origin main && git push origin main
 		echo -e "$success_message Commit '$1' sent successfully"
