3b61ef989b52f2ca33d67f7c68a39e98488b3630
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-04-30 17:37:17 -0500

    update /employee api

--- src/pages/api/employees/index.js
+++ src/pages/api/employees/index.js
@@ -1,85 +1,46 @@
-import { queryDB } from '../../../services/database'
 import { authHandler } from '../../../utils/authHandler'
-
 import { queryDatabase } from '../../../services/database'
-async function queryRoles(since, until) {
-	try {
-		const query = {
-			text: `SELECT * FROM get_customers($1, $2);`,
-			values: [since, until],
-		}
-		const result = await queryDB(query)
-		if (!result?.rows || result?.rows?.length < 1)
-			throw {
-				success: false,
-				message: 'Lo sentimos, no se ha podido procesar la solicitud, intente más tarde.',
-			}
-		return { success: true, message: 'ok', result: result.rows }
-	} catch (error) {
-		return error
-	}
+
+const API_BY_METHOD = {
+	['DELETE']: deleteEmployee,
+	['POST']: getEmployees,
+	['PUT']: updateEmployee,
 }
 
-async function deleteUser(id) {
-	try {
-		const query = {
-			text: `delete FROM customers where customer_id=$1;`,
-			values: [id],
-		}
-		const result = await queryDB(query)
-		if (!result)
-			throw {
-				success: false,
-				message: 'Lo sentimos, no se ha podido procesar la solicitud, intente más tarde.',
-			}
-		return { success: true, message: 'Borrado con éxito' }
-	} catch (error) {
-		return error
+async function deleteEmployee(req) {
+	const { id } = req.query
+	if (!id)
+		return res.status(404).json({ message: 'Petición rechazada, no se encontró el id' })
+	const query = { text: 'delete FROM customers where customer_id=$1;', values: [id] }
+	return await queryDatabase(query)
+}
+
+async function getEmployeeByDNI(dni) {
+	const query = {
+		text: 'select customer_dni from customers where customer_dni=$1;',
+		values: [dni],
 	}
+	return await queryDatabase(query, true)
+}
+
+async function getEmployees(req) {
+	if (req.query?.dni) return await getEmployeeByDNI(req.query?.dni)
+
+	const { since, until } = req.body
+	if (!since || !until) {
+		return { success: false, message: 'Petición rechaza, el rango de fecha es inválido' }
+	}
+	const query = { text: 'SELECT * FROM get_customers($1, $2);', values: [since, until] }
+	return await queryDatabase(query)
+}
+
+async function updateEmployee() {
+	return { success: true, message: 'ok' }
 }
 
 export default async function handler(req, res) {
 	const isAuth = authHandler(req.cookies)
-	if (!isAuth.success) {
-		res.status(400).json(isUserAuth.result)
-		return
-	}
-
-	if (req.method === 'DELETE') {
-		const { id } = req.query
-		if (!id)
-			return res.status(404).json({ message: 'Petición rechazada, no se encontró el id' })
-
-		const result = await deleteUser(id)
-		if (!result.success) {
-			res.status(404).json({ message: result.message })
-			return
-		}
-		return res.status(200).json(result)
-	}
-	if (req.method === 'POST') {
-		if (req.query?.dni) {
-			const { dni } = req.query
-			const query = {
-				text: 'select customer_dni from customers where customer_dni=$1;',
-				values: [dni],
-			}
-			const result = await queryDatabase(query, true)
-			if (result.success) return res.status(200).json(result)
-			return res.status(400).json({ message: 'Usuario no encontrado' })
-		}
-		const { since, until } = req.body
-		if (!since || !until) {
-			res.status(400).json({ message: 'Petición rechazada' })
-			return
-		}
-		const result = await queryRoles(since, until)
-		if (!result.success) {
-			res.status(404).json(result)
-			return
-		}
-		return res.status(200).json(result)
-	}
-
-	return res.status(404).json({ message: 'Método no permitido, petición rechazada' })
+	if (!isAuth.success) return res.status(400).json(isUserAuth.result)
+	const response = await API_BY_METHOD[req.method](req)
+	return res.status(response.success ? 200 : 400).json(response)
 }
