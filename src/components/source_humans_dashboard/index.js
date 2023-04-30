db7c8877df556bb94c7bb87fd4fcf162f5604906
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-04-29 20:36:12 -0500

    update navbar styles

--- src/components/source_humans_dashboard/index.js
+++ src/components/source_humans_dashboard/index.js
@@ -1,7 +1,36 @@
 import { useHook } from './useHook.js'
 
-export function SourceHumansDashboard(){
+export function SourceHumansDashboard() {
 	const { myClass } = useHook()
 
-	return <div className={myClass}>Hello world!</div>
+	return (
+		<main className={myClass}>
+			<h1>Dashboard</h1>
+			<section>
+				<header>
+					<h2>Recursos humanos</h2>
+				</header>
+				<article>
+					<h5>Contratados</h5>
+					<small>40</small>
+				</article>
+				<article>
+					<h5>Encomienda</h5>
+					<small>40</small>
+				</article>
+				<article>
+					<h5>Boletería</h5>
+					<small>40</small>
+				</article>
+				<article>
+					<h5>Contabilidad</h5>
+					<small>40</small>
+				</article>
+				<article>
+					<h5>Sistemas</h5>
+					<small>40</small>
+				</article>
+			</section>
+		</main>
+	)
 }
